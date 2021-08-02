57f864b29dda3741edd2268e93bf63ab4f827eb0
Mountaingoat-Hope <whtoa5@gmail.com>
2021-08-02 07:59:49 +0530

    User_vote lists page completed

--- page.py
+++ page.py
@@ -2,52 +2,46 @@ import sys
 import psycopg2
 import csv
 from flask import Flask,render_template,request
+import psycopg2
 
 app=Flask("Vote")
 
 
 @app.route("/")
 def base():
-  return render_template("base.html")
+  dbconn=psycopg2.connect("dbname=Votepool")
+  cursor=dbconn.cursor()
+  fname=sys.argv[1]
+  f=open(fname)
+  reader=csv.reader(f)
+  el=[]
+  li=[]
+  for title,purpose,expiry in reader:
+    el.append(title)
+    el.append(purpose)
+    el.append(int(expiry))
+    li.append(el)
+    el=[]
+  
+  h='''cursor.execute("SELECT name,username,pw FROM Auth")
+  for name,username,password in cursor.fetchall():
+       tu.append(name)
+       tu.append(username)
+       tu.append(password)
+       li.append(tu)
+  dbconn.commit()'''
+  
+  user_votelist=["Students","Elective course","Branch represenative"]
+  return render_template("page.html",user_votelist=li)
 
-@app.route("/insert")
-def insert(fname):
-   dbconn=psycopg2.connect("dbname=Votepool")
-   cursor=dbconn.cursor()
-   f=open(fname)
-   reader=csv.reader(f)
-   
-   for name,username,password in reader:
-      cursor.execute("Insert into Auth(name, username, pw) Values (%s, %s, %s)",(name, username, password))
-   dbconn.commit()
-   #print("Successfully committed")
-   f.close()
 
-@app.route("/dump")
-def dump(fname):
-   dbconn=psycopg2.connect("dbname=Votepool")
-   cursor=dbconn.cursor()
-   f=open(fname, "w")
-   writer=csv.writer(f)
-   
-   cursor.execute("SELECT name,username,pw FROM Auth WHERE id=3")
-   for name,username,password in cursor.fetchall():
-     print(name, username,   password)
-   dbconn.commit()
-   #print("Successfully selected")
-   f.close()
-   
+
 def main():
    fname=sys.argv[1]
    #insert(fname)
    dump(fname)
    
 #main()
-def addit():
-   x=3
-   y=4
-   print("added addition complete")
-   return x+y
 
 if __name__=="__main__":
    #print("main is called")
